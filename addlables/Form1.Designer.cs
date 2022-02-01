1dbb957abb94088baf451c870ec0664b005efcaa
dell <dell@DESKTOP-BD31QAE>
2022-02-01 19:16:56 +0800

    添加项目文件。

---
+++ addlables/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace addlables
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label90 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pan = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(967, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label90
+            // 
+            this.label90.AutoSize = true;
+            this.label90.Location = new System.Drawing.Point(623, 31);
+            this.label90.Name = "label90";
+            this.label90.Size = new System.Drawing.Size(55, 15);
+            this.label90.TabIndex = 1;
+            this.label90.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(857, 15);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(195, 274);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(623, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(967, 358);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 22);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pan
+            // 
+            this.pan.AutoScroll = true;
+            this.pan.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pan.Location = new System.Drawing.Point(12, 12);
+            this.pan.Name = "pan";
+            this.pan.Size = new System.Drawing.Size(206, 409);
+            this.pan.TabIndex = 5;
+            // 
+            // listView1
+            // 
+            this.listView1.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(733, 15);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(100, 279);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(967, 404);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 34);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(814, 336);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 44);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(307, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(294, 279);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(813, 398);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(131, 40);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "图片";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(694, 341);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(86, 47);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "sound";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(696, 410);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(84, 22);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "pause";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1074, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.pan);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label90);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label90;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel pan;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ addlables/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace addlables
+{
+
+    public partial class Form1 : Form
+    {
+        static int i = 0;
+        static clsMCI cm;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+           
+            Random rd = new Random();
+            int i2 = rd.Next();
+            string at = i2.ToString();
+           Label lbl = new Label();//声明一个label
+         //   lbl.Location = new System.Drawing.Point(0, i);//设置位置
+            
+            //    lbl.Size = new Size(20, 30);//设置大小
+            lbl.Text = at;//设置Text值
+            lbl.Name = "label3";
+            
+
+            int ch=0, cy=0;
+            Control.ControlCollection sonControls = this.pan .Controls;
+            //遍历所有控件
+            foreach (Control control in sonControls)
+            {
+                //  listBox1.Items.Add(control.Name);
+
+                cy=control.Location.Y;
+                
+                ch=control.Size.Height;
+            }
+
+            lbl.Location = new Point(0,cy+ch+10);
+            pan.Controls.Add(lbl);
+            lbl.Focus();
+     //       int hua = pan.VerticalScroll.Maximum;
+         //   Console.WriteLine(hua);
+           //     pan.VerticalScroll.LargeChange = hua;
+         //   pan.Controls..Add(lbl);//在当前窗体上添加这个label控件
+          //  Point location = label90.Location;
+          //  Console.WriteLine(location);
+        
+        }
+        private void GetControls1(Control fatherControl)
+        {
+            Control.ControlCollection sonControls = fatherControl.Controls;
+            //遍历所有控件
+            foreach (Control control in sonControls)
+            {
+              //  listBox1.Items.Add(control.Name);
+                
+                Console.WriteLine(control.Location);
+                Console.WriteLine(control.Name);
+                Console.WriteLine(control.Size);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GetControls1(this.pan);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            int i2 = rd.Next();
+            string at = i2.ToString();
+            
+            this.listView1.View = View.List;
+            listView1.Items.Add(at);
+            Label lbl = new Label();
+            lbl.Name = "label3";
+            lbl.Text = at;
+
+            listBox1.Items.Add(lbl.Text);
+            Label lb1 = new Label();//声明一个label
+                                    //   lbl.Location = new System.Drawing.Point(0, i);//设置位置
+
+            //    lbl.Size = new Size(20, 30);//设置大小
+            lbl.Text = "helloworld111111111111";//设置Text值
+            lbl.Name = "label3";
+            int h = lbl.Size.Height;
+            lbl.Location = new Point(0, 0);
+            pan.Controls.Add(lbl);
+
+
+
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+            
+            Random rd = new Random();
+            int i2 = rd.Next();
+            string at = i2.ToString();
+            Label lbl = new Label();
+            //声明一个label
+            //   lbl.Location = new System.Drawing.Point(0, i);//设置位置
+
+            //    lbl.Size = new Size(20, 30);//设置大小
+            lbl.Text = at;//设置Text值
+            lbl.Name = "label3";
+            lbl.BorderStyle = BorderStyle.FixedSingle;
+
+
+            int ch = 0, cy = 0;
+            Control.ControlCollection sonControls = this.pan.Controls;
+            //遍历所有控件
+            foreach (Control control in sonControls)
+            {
+                //  listBox1.Items.Add(control.Name);
+
+                cy = control.Location.Y;
+
+                ch = control.Size.Height;
+            }
+
+            lbl.Location = new Point(0, cy + ch + 10);
+            pan.Controls.Add(lbl);
+            lbl.Focus();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(@"b2a.jpg");
+            
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            clsMCI cm = new clsMCI();
+           if (i == 0)
+            { 
+                cm.FileName = @"C:\Users\dell\Desktop\3908406844.mp3";
+                cm.play();
+                 i = 2;
+
+
+            }
+            else if(i==2)
+            {
+                cm.Puase();
+                i = 3;           
+                    }
+            else
+            {
+                cm.play();
+            }
+                    
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            clsMCI cm = new clsMCI();
+            cm.Puase();
+            
+        }
+    }
+    public class clsMCI
+    {
+        public clsMCI()
+        {
+        }
+        //定义API函数使用的字符串变量 
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+        private string Name = "";
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+        private string durLength = "";
+        [MarshalAs(UnmanagedType.LPTStr, SizeConst = 128)]
+        private string TemStr = "";
+        int ilong;
+        //定义播放状态枚举变量
+        public enum State
+        {
+            mPlaying = 1,
+            mPuase = 2,
+            mStop = 3
+        };
+        //结构变量
+        public struct structMCI
+        {
+            public bool bMut;
+            public int iDur;
+            public int iPos;
+            public int iVol;
+            public int iBal;
+            public string iName;
+            public State state;
+        };
+        public structMCI mc = new structMCI();
+        //取得播放文件属性
+        public string FileName
+        {
+            get
+            {
+                return mc.iName;
+            }
+            set
+            {
+                try
+                {
+                    TemStr = "";
+                    TemStr = TemStr.PadLeft(127, Convert.ToChar(" "));
+                    Name = Name.PadLeft(260, Convert.ToChar(" "));
+                    mc.iName = value;
+                    ilong = APIClass.GetShortPathName(mc.iName, Name, Name.Length);
+                    Name = GetCurrPath(Name);
+                    Name = "open " + Convert.ToChar(34) + Name + Convert.ToChar(34) + " alias media";
+                    ilong = APIClass.mciSendString("close all", TemStr, TemStr.Length, 0);
+                    ilong = APIClass.mciSendString(Name, TemStr, TemStr.Length, 0);
+                    ilong = APIClass.mciSendString("set media time format milliseconds", TemStr, TemStr.Length, 0);
+                    mc.state = State.mStop;
+                }
+                catch
+                {
+                }
+            }
+        }
+        //播放
+        public void play()
+        {
+            TemStr = "";
+            TemStr = TemStr.PadLeft(127, Convert.ToChar(" "));
+            APIClass.mciSendString("play media", TemStr, TemStr.Length, 0);
+            mc.state = State.mPlaying;
+        }
+        //停止
+        public void StopT()
+        {
+            TemStr = "";
+            TemStr = TemStr.PadLeft(128, Convert.ToChar(" "));
+            ilong = APIClass.mciSendString("close media", TemStr, 128, 0);
+            ilong = APIClass.mciSendString("close all", TemStr, 128, 0);
+            mc.state = State.mStop;
+        }
+        public void Puase()
+        {
+            TemStr = "";
+            TemStr = TemStr.PadLeft(128, Convert.ToChar(" "));
+            ilong = APIClass.mciSendString("pause media", TemStr, TemStr.Length, 0);
+            mc.state = State.mPuase;
+        }
+        private string GetCurrPath(string name)
+        {
+            if (name.Length < 1) return "";
+            name = name.Trim();
+            name = name.Substring(0, name.Length - 1);
+            return name;
+        }
+        //总时间
+        public int Duration
+        {
+            get
+            {
+                durLength = "";
+                durLength = durLength.PadLeft(128, Convert.ToChar(" "));
+                APIClass.mciSendString("status media length", durLength, durLength.Length, 0);
+                durLength = durLength.Trim();
+                if (durLength == "") return 0;
+                return (int)(Convert.ToDouble(durLength) / 1000f);
+            }
+        }
+        //当前时间
+        public int CurrentPosition
+        {
+            get
+            {
+                durLength = "";
+                durLength = durLength.PadLeft(128, Convert.ToChar(" "));
+                APIClass.mciSendString("status media position", durLength, durLength.Length, 0);
+                mc.iPos = (int)(Convert.ToDouble(durLength) / 1000f);
+                return mc.iPos;
+            }
+        }
+    }
+    public class APIClass
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetShortPathName(
+         string lpszLongPath,
+         string shortFile,
+         int cchBuffer
+      );
+        [DllImport("winmm.dll", EntryPoint = "mciSendString", CharSet = CharSet.Auto)]
+        public static extern int mciSendString(
+           string lpstrCommand,
+           string lpstrReturnString,
+           int uReturnLength,
+           int hwndCallback
+          );
+    }
+}
